a320a9ab1f9abf82ed9b036887df315e9b2e2547
Laurin Sörensen <49350541+Tungdril@users.noreply.github.com>
2024-10-19 17:48:48 +0200

    added basic sqlite3 functionality

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 from pyscript import display
 from datetime import datetime
+import pyodide_js
+import asyncio
 
-now = datetime.now()
-display(now.strftime("%m/%d/%Y, %H:%M:%S"))
+async def main():
+    await pyodide_js.loadPackage("micropip")
+    import micropip
+    await micropip.install("sqlite3")
+    import sqlite3
+
+    now = datetime.now()
+    display(now.strftime("%m/%d/%Y, %H:%M:%S"))
+
+    print("Hello World!")
+    #connection = sqlite3.connect("/databases/users.db")
+
+    connection = sqlite3.connect(":memory:")
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT)")
+    cursor.execute("INSERT INTO users (name) VALUES ('Alice')")
+    connection.commit()
+
+    cursor.execute("SELECT * FROM users")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    connection.close()
+
+asyncio.ensure_future(main())
